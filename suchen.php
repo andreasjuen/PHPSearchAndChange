b100d6bbd1aecd54fba2b2e534bb0b5557c7fb3a
andreasjuen <andjuen@tsn.at>
2017-01-12 15:26:26 +0100

    Test

---
+++ suchen.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Suchen</title>
+	<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1" />
+</head>
+<body>
+	<h1>Suchen</h1>
+	<form action="verarbeitungSQL.php" method="get">
+
+		Nachname: <br />
+		<input type="text" name="nachname" size="20" maxlength="30" />
+		<br />
+
+		<input type="submit" value="suchen" />
+	</form>
+</body>	
+</html>
